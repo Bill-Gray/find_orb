f445967b333af4dec02106b2aa13a634c6a4e274
Bill-Gray <pluto@projectpluto.com>
2018-03-03 21:06:58 -0500

    Lots of GPL licences for files that really should have had them from the beginning

--- cssfield.cpp
+++ cssfield.cpp
@@ -1,3 +1,20 @@
+/* Copyright (C) 2018, Project Pluto
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA. */
+
 /*
 Support code for a new Find_Orb feature:  the ability to integrate an
 orbit backward to find out on which field(s) you might have caught it
