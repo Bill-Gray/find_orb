7743204a0aacf04e65839896f13d67e069e306c2
Bill-Gray <pluto@projectpluto.com>
2018-08-19 17:42:52 -0400

    In older MSVCs,  we now get snprintf() from the 'lunar' library (see snprintf.cpp in that repository).

--- ephem0.cpp
+++ ephem0.cpp
@@ -144,35 +144,10 @@ int parallax_to_lat_alt( const double rho_cos_phi, const double rho_sin_phi,
 
 #include <stdarg.h>
 #if defined(_MSC_VER) && _MSC_VER < 1900
-     /* Microsoft Visual C/C++ has no snprintf.  Yes,  you read that      */
-     /* correctly.  MSVC has an _snprintf which doesn't add a '\0' at the */
-     /* end if max_len bytes are written.  You can't pass a NULL string   */
-     /* to determine the necessary buffer size.  The following, however,  */
-     /* is a "good enough" replacement:  for a non-NULL string, the       */
-     /* output will be "correct" (the '\0' will always be present and in  */
-     /* the right place).  The only deviation from "proper" snprintf() is */
-     /* that the maximum return value is max_len;  you can never know how */
-     /* many bytes "should" have been written.                            */
-     /*   Somewhat ancient MSVCs don't even have vsnprintf;  you have to  */
-     /* use vsprintf and work things out so you aren't overwriting the    */
-     /* end of the buffer.                                                */
-
-int snprintf( char *string, const size_t max_len, const char *format, ...)
-{
-   va_list argptr;
-   int rval;
-
-   va_start( argptr, format);
-#if _MSC_VER <= 1100
-   rval = vsprintf( string, format, argptr);
-#else
-   rval = vsnprintf( string, max_len, format, argptr);
+                      /* For older MSVCs,  we have to supply our own  */
+                      /* snprintf().  See snprintf.cpp for details.  */
+int snprintf( char *string, const size_t max_len, const char *format, ...);
 #endif
-   string[max_len - 1] = '\0';
-   va_end( argptr);
-   return( rval);
-}
-#endif    /* #ifdef _MSC_VER */
 
 /* I find myself frequently snprintf()-ing at the end of a string,  with    */
 /* something like snprintf( str + strlen( str), sizeof( str) - strlen(str), */
