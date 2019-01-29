d6201473e24f586929bfecb708104d8365477e05
Bill-Gray <pluto@projectpluto.com>
2019-01-29 13:18:48 -0500

    Replaced my home-built strlcpy() with the OpenBSD one (yeah,  shoulda done it that way to begin with) and added strlcat().

--- miscell.cpp
+++ miscell.cpp
@@ -187,24 +187,6 @@ FILE *fopen_ext( const char *filename, const char *permits)
    return( rval);
 }
 
-#ifndef strlcpy
-
-      /* Some systems (BSD, Solaris,  others) offer this handy function. */
-      /* Linux and Windows don't.                                        */
-
-size_t strlcpy(char *dest, const char *src, size_t size)
-{
-   size_t i;
-
-   for( i = 0; i < size && *src; i++)
-      *dest++ = *src++;
-   if( i == size)
-      dest--;
-   *dest = '\0';
-   return( i);
-}
-#endif
-
 static int desig_matches( const char *iline, const char *desig)
 {
    const size_t len = strlen( desig);
@@ -395,4 +377,90 @@ uint64_t parse_bit_string( const char *istr)
          }
       }
    return( rval);
+}
+
+/* strlcat() and strlcpy() appear in some BSDs,  but I don't think they
+appear anyplace else (though in my opinion,  they should).   */
+
+/* $OpenBSD: strlcpy.c,v 1.16 2019/01/25 00:19:25 millert Exp $   */
+
+/*
+ * Copyright (c) 1998, 2015 Todd C. Miller <millert@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * Copy string src to buffer dst of size dsize.  At most dsize-1
+ * chars will be copied.  Always NUL terminates (unless dsize == 0).
+ * Returns strlen(src); if retval >= dsize, truncation occurred.
+ */
+size_t strlcpy( char *dst, const char *src, size_t dsize)
+{
+   const char *osrc = src;
+   size_t nleft = dsize;
+
+   /* Copy as many bytes as will fit. */
+   if (nleft != 0) {
+      while (--nleft != 0) {
+         if ((*dst++ = *src++) == '\0')
+            break;
+      }
+   }
+
+   /* Not enough room in dst, add NUL and traverse rest of src. */
+   if (nleft == 0) {
+      if (dsize != 0)
+         *dst = '\0';      /* NUL-terminate dst */
+      while (*src++)
+         ;
+   }
+
+   return(src - osrc - 1); /* count does not include NUL */
+}
+
+
+/*
+ * Appends src to string dst of size dsize (unlike strncat, dsize is the
+ * full size of dst, not space left).  At most dsize-1 characters
+ * will be copied.  Always NUL terminates (unless dsize <= strlen(dst)).
+ * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
+ * If retval >= dsize, truncation occurred.
+ */
+
+size_t strlcat( char *dst, const char *src, size_t dsize)
+{
+   const char *odst = dst;
+   const char *osrc = src;
+   size_t n = dsize;
+   size_t dlen;
+
+   /* Find the end of dst and adjust bytes left but don't go past end. */
+   while (n-- != 0 && *dst != '\0')
+      dst++;
+   dlen = dst - odst;
+   n = dsize - dlen;
+
+   if (n-- == 0)
+      return(dlen + strlen(src));
+   while (*src != '\0') {
+      if (n != 0) {
+         *dst++ = *src;
+         n--;
+      }
+      src++;
+   }
+   *dst = '\0';
+
+   return(dlen + (src - osrc));  /* count does not include NUL */
 }
