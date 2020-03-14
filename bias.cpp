a746648fc65474c37a46cc38a882425a0cd64551
Bill-Gray <pluto@projectpluto.com>
2020-03-13 21:00:38 -0400

    Debiasing can now be done with either FCCT14 (as before) or with the new EFCC18 debiasing tables.  Formats are similar,  but the 19 FCCT14 catalogues were hard-wired;  the function now actually checks to see how many catalogues are listed and what their codes are,  which did make for a lot of revision.

--- bias.cpp
+++ bias.cpp
@@ -34,21 +34,29 @@ int find_fcct_biases( const double ra, const double dec, const char catalog,
 
 /* Compute star catalog position/proper motion biases,  from tables by
 Farnocchia,  Chesley,  Chamberlin,  Tholen,  _Icarus_ 245 (2015) 94-111,
-http://adsabs.harvard.edu/abs/2015Icar..245...94F  ,  "FCCT14".  The
-actual debiasing file,  'bias.dat',  is in
+http://adsabs.harvard.edu/abs/2015Icar..245...94F  ,  "FCCT14";  or
+from its successor,  Eggl,  Farnocchia, Chamberlin, Chesley,  "EFCC18",
+https://arxiv.org/abs/1909.04558.  You should really use the latter;
+FCCT14 is now largely of historical interest.
+
+   The actual debiasing files,  named 'bias.dat' for both,  are in
 
 ftp://ssd.jpl.nasa.gov/pub/ssd/debias/debias_2014.tgz
+ftp://ssd.jpl.nasa.gov/pub/ssd/debias/debias_2018.tgz
 
-   The .tgz file contains two other files,  README.txt and tiles.dat.  As
+   The .tgz files contain two other files,  README.txt and tiles.dat.  As
 far as Find_Orb is concerned,  you can ignore these.
 
-   FCCT14 divides the sky into 12 * 64 * 64 = 49152 "tiles" of equal area and
-passably square shape,  using the HEALPIX tesselation (see 'healpix.cpp').
-Within each tile, the bias of 19 different catalogs in RA, dec,  and proper
-motion in RA and dec are given.  (The biases are relative to a "presumed good
-reference" subset of PPMXL.)  Those biases are stored in 'bias.dat'.
+   In both FCCT14 and EFCC18,  the sky is divided into 12 * 64 * 64 =
+49152 "tiles" of equal area and passably square shape,  using the HEALPIX
+tesselation (see 'healpix.cpp'). Within each tile, the bias of 19 (for
+FCCT14) or 26 (for EFCC18) different catalogs in RA, dec,  and proper
+motion in RA and dec are given.  The biases are relative to a "presumed
+good reference".  For FCCT14,  that was subset of PPMXL.  ECFF18 was
+able to use Gaia-DR2,  an improvement.)  Those biases are then stored in
+'bias.dat'.
 
-   This code reads in all 49152 * 19 biases (each of which is really
+   This code reads in all 49152 * n_cats biases (each of which is really
 four biases,  one each in RA,  dec,  pm_RA,  pm_dec).  For a given
 position,  it figures out the tile covering that point,  then extracts
 the biases corresponding to the catalog in which we're interested and
@@ -64,11 +72,11 @@ int find_fcct_biases( const double ra, const double dec, const char catalog,
                  const double jd, double *bias_ra, double *bias_dec)
 {
    static int16_t *bias_data = NULL;
-   static const char *catalog_codes = "abcdegijlmopqruvwLN";
-   const char *catalog_loc = strchr( catalog_codes, catalog);
+   static char catalog_codes[30];
+   const char *catalog_loc;
          /* Number of HEALPIX tiles at level N=64 : */
    const int n_tiles = 12 * 64 * 64;
-   const int n_cats = 19;
+   static int n_cats = 0;
    int loc, i;
    double x, y, year;
    const double j2000 = 2451545.;      /* JD 2451545.0 = 2000 Jan 1.5 */
@@ -83,25 +91,30 @@ int find_fcct_biases( const double ra, const double dec, const char catalog,
       }
 
    *bias_ra = *bias_dec = 0.;
-   if( !catalog_loc)    /* don't have bias data for this catalog */
-      return( BIAS_NO_DATA_FOR_THAT_CATALOG);
    if( bias_file_known_to_be_missing)
       return( BIAS_NO_BIAS_FILE);
    if( !bias_data)
       {
       FILE *ifile;
-      const char *crunched_fcct14_file_name = "bias_bin.dat";
-      size_t nbytes;
-      const size_t csize = n_tiles * n_cats * 4 * sizeof( int16_t);
-               /* The above "compressed size" corresponds to 12*64*64*19*4
-                  = 3735552 short ints, or 7471104 bytes. */
+      const char *crunched_fcct14_file_name = "biases.bin";
+      size_t nbytes, csize = 0;
 
-      bias_data = (int16_t *)malloc( csize + 1);
-      if( !bias_data)
-         return( BIAS_NOT_ALLOCATED);
       ifile = fopen_ext( crunched_fcct14_file_name, "crb");
       if( ifile)     /* not our first time:  we have a compressed file */
          {
+         unsigned char ncats;
+
+         if( fread( &ncats, 1, 1, ifile) != 1)
+            return( BIAS_FILE_WRONG_SIZE);
+         n_cats = (int)ncats;
+         assert( n_cats == 19 || n_cats == 26);
+         if( fread( catalog_codes, 1, n_cats, ifile) != (size_t)n_cats)
+            return( BIAS_FILE_WRONG_SIZE);
+         catalog_codes[n_cats] = '\0';
+         csize = n_tiles * n_cats * 4 * sizeof( int16_t);
+         bias_data = (int16_t *)malloc( csize + 1);
+         if( !bias_data)
+            return( BIAS_NOT_ALLOCATED);
          nbytes = fread( bias_data, 1, csize + 1, ifile);
          fclose( ifile);
          if( nbytes != csize)
@@ -109,7 +122,7 @@ int find_fcct_biases( const double ra, const double dec, const char catalog,
          }
       else
          {
-         char buff[600];   /* first time: read ASCII file & binary-ize; */
+         char buff[900];   /* first time: read ASCII file & binary-ize; */
          FILE *ofile;     /* store binary version for all future use   */
 
          ifile = NULL;
@@ -126,7 +139,7 @@ int find_fcct_biases( const double ra, const double dec, const char catalog,
             }
          loc = 0;
          while( loc < n_tiles && fgets( buff, sizeof( buff), ifile))
-            if( *buff != '!')
+            if( *buff != '!' && bias_data)
                {
                char *tptr = buff;
                int16_t *bptr = bias_data + loc * n_cats * 4;
@@ -154,14 +167,42 @@ int find_fcct_biases( const double ra, const double dec, const char catalog,
                   }
                loc++;
                }
+            else if( !memcmp( buff, "! |---", 6))
+               {        /* header line */
+               n_cats = strlen( buff) / 29;
+               assert( n_cats == 19 || n_cats == 26);
+               for( i = 0; i < n_cats; i++)
+                  {
+                  char *tptr = buff + 22 + i * 29;
+
+                  assert( *tptr == ' ' && tptr[1] == '-');
+                  while( tptr > buff && *tptr == ' ')
+                     tptr--;
+                  catalog_codes[i] = *tptr;
+                  }
+               catalog_codes[n_cats] = '\0';
+               csize = n_tiles * n_cats * 4 * sizeof( int16_t);
+               bias_data = (int16_t *)malloc( csize + 1);
+               if( !bias_data)
+                  return( BIAS_NOT_ALLOCATED);
+               }
          fclose( ifile);
          assert( loc == n_tiles);
+
          ofile = fopen_ext( crunched_fcct14_file_name, "fcwb");
+         *buff = (char)n_cats;
+         nbytes = fwrite( buff, 1, 1, ofile);
+         assert( nbytes == 1);
+         nbytes = fwrite( catalog_codes, 1, n_cats, ofile);
+         assert( nbytes == (size_t)n_cats);
          nbytes = fwrite( bias_data, 1, csize, ofile);
          assert( nbytes == csize);
          fclose( ofile);
          }
       }
+   catalog_loc = strchr( catalog_codes, catalog);
+   if( !catalog_loc)    /* don't have bias data for this catalog */
+      return( BIAS_NO_DATA_FOR_THAT_CATALOG);
    ra_dec_to_xy( ra, dec, &x, &y);
    loc = xy_to_healpix( x, y, 64);
 // debug_printf( "catalog %c, tile %d: ", catalog, loc);
