1164be372acb51ad348703f4ff666999eb1951ae
Bill-Gray <pluto@projectpluto.com>
2022-02-23 16:16:59 -0500

    Updated version text

--- miscell.cpp
+++ miscell.cpp
@@ -747,6 +747,6 @@ int snprintf_err( char *string, const size_t max_len,      /* miscell.cpp */
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2459626.5;
-    return( "2021 Feb 16");
+      *jd = 2459633.5;
+    return( "2021 Feb 23");
 }
