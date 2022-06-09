00a494921b3835cf2ba3267dbe8194c364819d44
Bill-Gray <pluto@projectpluto.com>
2022-06-09 11:27:51 -0400

    Added some permanent IDs for irregular Saturn satellites

--- miscell.cpp
+++ miscell.cpp
@@ -758,6 +758,6 @@ int snprintf_err( char *string, const size_t max_len,      /* miscell.cpp */
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2459732.5;
-    return( "2022 Jun 02");
+      *jd = 2459739.5;
+    return( "2022 Jun 09");
 }
