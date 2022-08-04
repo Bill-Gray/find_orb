0a6d5e5fa54c9f927928b1b013a345daf81177cc
Bill-Gray <pluto@projectpluto.com>
2022-08-04 11:12:51 -0400

    ADES sigmas should be (and now are) reset after the observation is read in.  The way I had it,  they only got reset when an observation _of the particular object_ was read in.  If you had ADES data for X in a file followed by 80-column data for Y,  the first Y observation would get sigmas from the last X observation.

--- mpc_obs.cpp
+++ mpc_obs.cpp
@@ -3742,10 +3742,6 @@ OBSERVE FAR *load_observations( FILE *ifile, const char *packed_desig,
                   SET_SIGMA( posn_sigma_1, ades_posn_sigma_1, override_posn_sigma_1);
                   SET_SIGMA( posn_sigma_2, ades_posn_sigma_2, override_posn_sigma_2);
                   SET_SIGMA( posn_sigma_theta, ades_posn_sigma_theta, override_posn_sigma_theta);
-                           /* Sigmas from ADES or Dave Tholen apply to only */
-                           /* one observation.  Zero 'em out after that use : */
-                  ades_posn_sigma_1 = ades_posn_sigma_2 = ades_mag_sigma = 0.;
-                  ades_posn_sigma_theta = ades_time_sigma = 0.;
 
                   if( sscanf( rval[i].columns_57_to_65, "%lf %lf%n",
                               &ra_sigma, &dec_sigma, &bytes_read) >= 2
@@ -3846,6 +3842,11 @@ OBSERVE FAR *load_observations( FILE *ifile, const char *packed_desig,
                spacecraft_offset_reference = 399;  /* default to geocentric offsets */
                }
             }
+         }
+      if( is_in_range( jd))     /* Sigmas from ADES or Dave Tholen apply to only */
+         {                 /* one observation.  Zero 'em out after that use : */
+         ades_posn_sigma_1 = ades_posn_sigma_2 = ades_mag_sigma = 0.;
+         ades_posn_sigma_theta = ades_time_sigma = 0.;
          }
       override_time = 0.;
       override_ra = override_dec = -100.;
