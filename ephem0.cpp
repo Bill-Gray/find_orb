1d96683421bbbd55a711cbdf448a1dc94eb99a5b
Bill-Gray <pluto@projectpluto.com>
2022-08-26 12:24:42 -0400

    Fixed a buffer overflow and removed an unneeded debug statement

--- ephem0.cpp
+++ ephem0.cpp
@@ -3569,19 +3569,18 @@ static void get_scope_params( const char *mpc_code, expcalc_config_t *c)
    FILE *ifile;
    int scope_details = EXPCALC_NO_CONFIG_FOUND;
    const char *scope_json_file = get_environment_ptr( "SCOPE_JSON_FILE");
-   char filename[20];
+   char filename[120];
 
    snprintf( filename, sizeof( filename), "site_%.3s.txt", mpc_code);
    ifile = fopen_ext( filename, "clrb");
    if( ifile)
       {
       scope_details = find_expcalc_config_from_mpc_code( mpc_code, ifile, c);
-      printf( "Searched '%s' : %d\n", filename, scope_details);
       fclose( ifile);
       }
    if( scope_details == EXPCALC_NO_CONFIG_FOUND && *scope_json_file)
       {
-      strcpy( filename, scope_json_file);
+      strlcpy_error( filename, scope_json_file);
       text_search_and_replace( filename, "%c", mpc_code);
       ifile = fopen_ext( filename, "rb");
       if( ifile)
