aba301e2464a9e4c48975de10ac96cc0cdf64b56
Bill-Gray <pluto@projectpluto.com>
2022-10-27 13:52:40 -0400

    Updated version constants

--- miscell.cpp
+++ miscell.cpp
@@ -622,6 +622,6 @@ const char *write_bit_string( char *ibuff, const uint64_t bits)
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2459869.5;
-    return( "2022 Oct 17");
+      *jd = 2459878.5;
+    return( "2022 Oct 26");
 }
