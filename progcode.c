7cfe9f3e36e6f057670c9dde966345a0eca76091
Bill-Gray <pluto@projectpluto.com>
2023-03-28 16:57:32 -0400

    Updated program code data,  plus the program that processes program codes (should cut down on the amount of manual editing required for future updates)

--- progcode.c
+++ progcode.c
@@ -30,6 +30,37 @@ is involved here,  and I'm not totally certain of the extended part.
 #include <stdio.h>
 #include <string.h>
 
+/* Returns 0 if the line was good,  or -1 if it should be expunged.
+The original 'ProgramCodes.txt' has several blunders in it. */
+
+static int correct_line( char *buff)
+{
+   size_t i = strlen( buff);
+   char *tptr;
+   const char *removers[6] = {
+               "568 1 D, C. Jewitt",
+               "658 4 JJ Kavelaars",
+               "G40 9 B. Luetkenhoener",
+               "Q62 } S. K\\\"urti",
+               "U69 # B. Luetkenhoener",
+               NULL };
+
+   while( i && buff[i - 1] <= ' ')
+      i--;
+   buff[i] = '\n';      /* remove trailing spaces */
+   buff[i + 1] = '\0';
+                  /* ad hoc fixes to blunders in original text : */
+   for( i = 0; removers[i]; i++)
+      if( strstr( buff, removers[i]))
+         return( -1);
+   tptr = strstr( buff, "JJ Kavelaars");
+   if( tptr)
+      strcpy( tptr, "J. J. Kavelaars\n");
+   if( strstr( buff, "D, C. Jewitt"))
+      return( -1);
+   return( 0);
+}
+
 int main( const int argc, const char **argv)
 {
    const char *filename = (argc == 1 ? "ProgramCodes.txt" : argv[1]);
@@ -46,7 +77,8 @@ int main( const int argc, const char **argv)
    while( fgets( buff, sizeof( buff), ifile))
       if( buff[0] > ' ' && buff[1] > ' ' && buff[2] > ' '
                         && buff[3] == ' ' && buff[4] > ' '
-                        && buff[5] == ' ' && buff[6] > ' ')
+                        && buff[5] == ' ' && buff[6] > ' '
+                        && !correct_line( buff))
          {
          if( memcmp( prev_code, buff, 5))
             {
