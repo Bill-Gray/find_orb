560c45752ccc8be4221c563945e806dbf94156c9
Bill-Gray <pluto@projectpluto.com>
2023-09-03 10:06:37 -0400

    Updated list of program codes,  and the snippet of C code used to generate that list from the MPC one.

--- progcode.c
+++ progcode.c
@@ -5,26 +5,33 @@ https://minorplanetcenter.net/iau/lists/ProgramCodes.txt
 and produce a file 'progcode.txt' suitable for Find_Orb to extract
 observer info from.  Note that some editing of the input (or output)
 is required; some observers appear twice,  with modifications due to
-diacritical marks.  When doing updates,  I compare to the previous
-version to make sure such problems don't creep back in.
+diacritical marks.  That's because MPC uses the file at the above URL
+to determine the program code associated with a given name,  and
+people may submit as (say) "G. Borisov",  then as "G. V. Borisov".
+I'm using the file to find the (canonical) name for a given program
+code,  and therefore drop the duplicates.
+
+When doing updates,  I compare to the previous version to make sure
+such problems don't creep back in.
 
    Compile with :
 
 gcc -Wall -pedantic -Wextra -O3 progcode.c -o progcode
 
-   Extension of the table provided at the above URL.  Some inference
-is involved here,  and I'm not totally certain of the extended part.
+   Extension of the table provided at the above URL.  The top line
+goes one character further to include the pound (currency) symbol,
+but I'd expect a non-7-bit-ASCII character to raise hell.
 
-10 !       20 +      30 `     40 ?      50 I      60 S
-11 "       21 ,      31 {     41 @      51 J      61 T
-12 #       22 -      32 |     42 A      52 K      62 U
-13 $       23 .      33 }     43 B      53 L      63 V
-14 %       24 /      34 ~     44 C      54 M
-15 &       25 [      35 :     45 D      55 N
-16 '       26 \      36 ;     46 E      56 O
-17 (       27 ]      37 <     47 F      57 P
-18 )       28 ^      38 =     48 G      58 Q
-19 *       29 _      39 >     49 H      59 R
+10 !    20 +    30 `    40 ?    50 I    60 S    70 c    80 m    90 w
+11 "    21 ,    31 {    41 @    51 J    61 T    71 d    81 n    91 x
+12 #    22 -    32 |    42 A    52 K    62 U    72 e    82 o    92 y
+13 $    23 .    33 }    43 B    53 L    63 V    73 f    83 p    93 z
+14 %    24 /    34 ~    44 C    54 M    64 W    74 g    84 q
+15 &    25 [    35 :    45 D    55 N    65 X    75 h    85 r
+16 '    26 \    36 ;    46 E    56 O    66 Y    76 i    86 s
+17 (    27 ]    37 <    47 F    57 P    67 Z    77 j    87 t
+18 )    28 ^    38 =    48 G    58 Q    68 a    78 k    88 u
+19 *    29 _    39 >    49 H    59 R    69 b    79 l    89 v
 */
 
 #include <stdio.h>
@@ -36,13 +43,17 @@ The original 'ProgramCodes.txt' has several blunders in it. */
 static int correct_line( char *buff)
 {
    size_t i = strlen( buff);
-   char *tptr;
-   const char *removers[6] = {
-               "568 1 D, C. Jewitt",
+   const char *removers[] = {
+               "095 2 V. Rumyantsev",
+               "095 4 G. Borisov",
+               "266 3 J. Kavelaars",
+               "267 3 J. Kavelaars",
+               "568 1 D. Jewitt",
                "658 4 JJ Kavelaars",
                "G40 9 B. Luetkenhoener",
                "Q62 } S. K\\\"urti",
                "U69 # B. Luetkenhoener",
+               "W84 \" J. Pena Z.",
                NULL };
 
    while( i && buff[i - 1] <= ' ')
@@ -53,11 +64,8 @@ static int correct_line( char *buff)
    for( i = 0; removers[i]; i++)
       if( strstr( buff, removers[i]))
          return( -1);
-   tptr = strstr( buff, "JJ Kavelaars");
-   if( tptr)
-      strcpy( tptr, "J. J. Kavelaars\n");
    if( strstr( buff, "D, C. Jewitt"))
-      return( -1);
+      buff[7] = '.';
    return( 0);
 }
 
