958d393c07824bf569c4b3c3e86f149bcf9e85e2
Bill-Gray <pluto@projectpluto.com>
2024-01-18 21:06:37 -0500

    Added three new obscodes,  renamed (N55) in Tibet,  MPC fixed the position for (073) Bucharest.

--- miscell.cpp
+++ miscell.cpp
@@ -670,6 +670,6 @@ int pattern_match(const char* pattern, const char* string)
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2460314.5;
-    return( "2024 Jan 05");
+      *jd = 2460327.5;
+    return( "2024 Jan 18");
 }
