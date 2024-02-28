7587ec800c4a3cb12eb9a63c50134da984409dc0
Bill-Gray <pluto@projectpluto.com>
2024-02-28 10:34:04 -0500

    Updated ObsCodes.htm,  version date

--- miscell.cpp
+++ miscell.cpp
@@ -670,6 +670,6 @@ int pattern_match(const char* pattern, const char* string)
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2460346.5;
-    return( "2024 Feb 06");
+      *jd = 2460368.5;
+    return( "2024 Feb 28");
 }
