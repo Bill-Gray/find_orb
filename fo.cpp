8df7d296866360362a84558445c05fbfe8b448b6
Bill-Gray <pluto@projectpluto.com>
2024-09-20 13:49:17 -0400

    Non-interactive Find_Orb in multiprocessor mode sometimes failed to find 'piece' files and didn't unlink temporary files

--- fo.cpp
+++ fo.cpp
@@ -164,31 +164,45 @@ static int remove_single_observation_objects( OBJECT_INFO *ids, const int n_ids)
 
 char *make_config_dir_name( char *oname, const char *iname);
 
+#ifdef _WIN32                /* MS is different. */
+   #define UNLINK       _unlink
+#else
+   #define UNLINK       unlink
+#endif
+
 #ifdef FORKING
 static int unlink_config_file( const char *filename)
 {
    char buff[255];
    int err_code;
    extern int use_config_directory;          /* miscell.c */
+   extern const char *output_directory;
 
    get_file_name( buff, filename);
-   if( use_config_directory)
+   if( output_directory && *output_directory)
+      {
+      char cpath[255];
+
+      strlcpy_error( cpath, output_directory);
+      strlcat_error( cpath, "/");
+      strlcat_error( cpath, buff);
+      err_code = UNLINK( cpath);
+      if( err_code)
+         fprintf( stderr, "Failed unlinking '%s' ('%s')\n", filename, buff);
+      }
+   else if( use_config_directory)
       {
       char cpath[255];
 
       make_config_dir_name( cpath, buff);
-#ifdef _WIN32                /* MS is different. */
-      err_code = _unlink( cpath);
-#else
-      err_code = unlink( cpath);
-#endif
+      err_code = UNLINK( cpath);
+      if( err_code)
+         fprintf( stderr, "Failed unlinking '%s'\n", cpath);
       }
    else
-#ifdef _WIN32
-      err_code = _unlink( buff);
-#else
-      err_code = unlink( buff);
-#endif
+      err_code = UNLINK( buff);
+   if( err_code)
+      fprintf( stderr, "Failed unlinking '%s' ('%s')\n", filename, buff);
    return( err_code);
 }
 
@@ -251,7 +265,10 @@ static void _merge_element_files( const char *filename, const int n_processes,
          {
          err_code = unlink_config_file( filename);
          if( err_code)
+            {
+            fprintf( stderr, "Failed to unlink '%s' ('%s')\n", buff, filename);
             perror( buff);
+            }
          assert( !err_code);
          }
       }
