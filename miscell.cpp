e8634b7cc8a5497cbeed25097d4f6277d8a257df
Bill-Gray <pluto@projectpluto.com>
2025-04-10 17:41:48 -0400

    Updated version date and a few default observer details

--- miscell.cpp
+++ miscell.cpp
@@ -669,6 +669,6 @@ int pattern_match(const char* pattern, const char* string)
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2460771.5;
-    return( "2025 Apr 06");
+      *jd = 2460775.5;
+    return( "2025 Apr 10");
 }
