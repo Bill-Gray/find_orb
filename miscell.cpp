1665530845d2796e728b76259d9c3b8016157e76
Bill-Gray <pluto@projectpluto.com>
2025-05-06 11:58:29 -0400

    Updated observatory codes and version info

--- miscell.cpp
+++ miscell.cpp
@@ -669,6 +669,6 @@ int pattern_match(const char* pattern, const char* string)
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2460795.5;
-    return( "2025 Apr 30");
+      *jd = 2460801.5;
+    return( "2025 May 06");
 }
