5b3a61f9736afdc74566ad18ecabdbdfc60c7105
Bill-Gray <pluto@projectpluto.com>
2025-07-14 10:12:41 -0400

    Updated obscodes,  version date,  splash screen,  and a few 'odd' names (mostly for interstellar objects)

--- miscell.cpp
+++ miscell.cpp
@@ -669,6 +669,6 @@ int pattern_match(const char* pattern, const char* string)
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2460865.5;
-    return( "2025 Jul 09");
+      *jd = 2460870.5;
+    return( "2025 Jul 14");
 }
