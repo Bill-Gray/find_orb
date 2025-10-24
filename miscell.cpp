6d9fd062d85bd82384edb9529f2e7795cee715b6
Bill-Gray <pluto@projectpluto.com>
2025-10-24 10:39:51 -0400

    Updated obscodes and version date

--- miscell.cpp
+++ miscell.cpp
@@ -707,6 +707,6 @@ int pattern_match(const char* pattern, const char* string)
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2460962.5;
-    return( "2025 Oct 14");
+      *jd = 2460969.5;
+    return( "2025 Oct 21");
 }
