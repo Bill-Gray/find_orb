0f117fc3e641007590345643e1757613726ad4a6
Bill-Gray <pluto@projectpluto.com>
2026-08-15 21:22:49 -0400

    Updated version date

--- miscell.cpp
+++ miscell.cpp
@@ -699,6 +699,6 @@ int pattern_match(const char* pattern, const char* string)
 const char *find_orb_version_jd( double *jd)
 {
     if( jd)
-      *jd = 2461243.5;
-    return( "2026 Jul 22");
+      *jd = 2461262.5;
+    return( "2026 Aug 10");
 }
